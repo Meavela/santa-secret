09363dabb0577a1eb99ffd83e6c1dcf6001444b0
Lou BEGE <lou.bege@epsi.fr>
2021-11-27 19:44:29 +0100

    pick random

--- add-ons/prompt/prompt.js
+++ add-ons/prompt/prompt.js
@@ -2,16 +2,21 @@ function launchPrompt(){
     $('#choicePrompt').modal({
         backdrop: false
     })
+
     dynamicSelectOptions();
+
     $('#toggle-prompt-choice').click();
 }
 
 function dynamicSelectOptions(){
     var select = "";
     var result = [];
-    for(var i in participants_json)
-        result.push([i, participants_json [i]]);
 
+    // transform json to array
+    for(var i in hasChoosen_json)
+        result.push([i, hasChoosen_json [i]]);
+
+    // dynamic selection
     for (let index = 0; index < result.length; index++) {
         if(result[index][1] == false){
             select += "<option>"+result[index][0]+"</option>";
@@ -21,3 +26,33 @@ function dynamicSelectOptions(){
     document.getElementById("selectChoicePrompt").innerHTML = select;
 }
 
+function chooseRandomPeople(){
+    var result = [];
+    var items = [];
+
+    // selected
+    var selected = document.getElementById("selectChoicePrompt").value
+    console.log(selected);
+
+    // transform json to array
+    for(var i in isChoosen_json)
+        result.push([i, isChoosen_json [i]]);
+
+    // dynamic selection
+    for (let index = 0; index < result.length; index++) {
+        if(result[index][1] == false && selected != result[index][0]){
+            items.push(result[index][0]);
+        }
+    }
+
+    // pick random
+    var item = items[Math.floor(Math.random()*items.length)];
+
+    // assign
+    hasChoosen_json[selected] = true;
+    isChoosen_json[item] = true;
+    
+    document.getElementById("choosen-participant").innerHTML = item;
+
+    $('#close-prompt-choice').click();
+}
--- participants.js
+++ participants.js
@@ -1,4 +1,13 @@
-participants_json = {
+isChoosen_json = {
+    "Olu": false,
+    "Elfie": false,
+    "Pierro": false,
+    "Gwen": false,
+    "Alexis": false,
+    "Lou": false,
+    "Thibaut": false
+}
+hasChoosen_json = {
     "Olu": false,
     "Elfie": false,
     "Pierro": false,
